eb5e85474003ebea825d28838679e3fa6c308e34
Haloegen <>
2023-07-19 11:22:50 +0000

    add style structure to game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,21 @@
 
-    const playerScore = 0;
-    const computerScore = 0;
-    const choices = ["rock", "paper", "scissors", "lizard", "spock"]
-    const playerImage = document.getElementById("player-image")
-    const computerImage = document.getElementById("computer-image")
+     let playerScore = 0;
+     let computerScore = 0;
+     let choices = ["rock", "paper", "scissors", "lizard", "spock"]
+     let playerImg = document.getElementById("player-select")
+     let comImg = document.getElementById("computer-select")
+     
 
+
+// This code generates the computers turn
     function computerPlay() {
-             randomIndex = Math.floor(Math.random() * choices.length);
+            let randomIndex = Math.floor(Math.random() * choices.length);
             return choices[randomIndex];
           }
     
-
+/* Creating a function with two parameters, this code then goes through every iteration and decided who won.
+It then updates a score, and puts the player and computers moves into modifying the image src property to display the image.
+*/
     function playRound(playerSelection, computerSelection) {
       let player = playerSelection;
       let computer = computerSelection;
@@ -40,7 +45,8 @@
       return `You lose! ${computer} beats ${player}.`;
       
     }
-
+    // plays the game, and runs through each function
+    
     function handleClick(event) {
       let playerSelection = event.target.id;
       let computerSelection = computerPlay();
@@ -48,7 +54,7 @@
       displayResult(playerSelection, computerSelection, result);
       updateScore();
     }
-
+//Code displays the moves for a screen reader to explain how and who won.
     function displayResult(playerMove, computerMove, result) {
       let playerMoveElement = document.getElementById("player-move");
       let computerMoveElement = document.getElementById("computer-move");
@@ -60,6 +66,7 @@
       resultElement.textContent = result;
     }
 
+    // updates score board to keep track of win/loss
     function updateScore() {
       let playerScoreElement = document.getElementById("player-score");
       let computerScoreElement = document.getElementById("computer-score");
@@ -68,6 +75,7 @@
       computerScoreElement.textContent = computerScore;
     }
 
+    // gives the buttons their functionality 
     let buttons = document.querySelectorAll("button");
     buttons.forEach(button => {
       button.addEventListener("click", handleClick);
