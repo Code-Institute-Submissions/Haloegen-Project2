ce795944fad106b62f3fb7d2b5526b0d158d78e5
Haloegen <>
2023-08-02 14:23:46 +0000

    added endgame and reset button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,97 @@
 
-     let playerScore = 0;
-     let computerScore = 0;
-     let choices = ["rock", "paper", "scissors", "lizard", "spock"]
-     let playerImg = document.getElementById("player-select")
-     let comImg = document.getElementById("computer-select")
-     
-
+let playerScore = 0;
+let computerScore = 0;
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+let playerImg = document.getElementById("player-select")
+let comImg = document.getElementById("computer-select");
+const buttons = document.querySelectorAll(".buttn");
+const resetButton.document.getElementById("reset")
+const winningScore = 10;
 
 // This code generates the computers turn
-    function computerPlay() {
-            let randomIndex = Math.floor(Math.random() * choices.length);
-            return choices[randomIndex];
-          }
-    
+function computerPlay() {
+    let randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
 /* Creating a function with two parameters, this code then goes through every iteration and decided who won.
 It then updates a score, and puts the player and computers moves into modifying the image src property to display the image.
 */
-    function playRound(playerSelection, computerSelection) {
-      let player = playerSelection;
-      let computer = computerSelection;
+function playRound(playerSelection, computerSelection) {
+    let player = playerSelection;
+    let computer = computerSelection;
 
-      if (player === computer) {
+    if (player === computer) {
         playerImg.src = `assets/images/${player}.png`;
         comImg.src = `assets/images/${computer}.png`;
         return "It's a tie!";
-      }
+    }
 
-      if (
+    if (
         (player === "rock" && (computer === "scissors" || computer === "lizard")) ||
         (player === "paper" && (computer === "rock" || computer === "spock")) ||
         (player === "scissors" && (computer === "paper" || computer === "lizard")) ||
         (player === "lizard" && (computer === "paper" || computer === "spock")) ||
         (player === "spock" && (computer === "rock" || computer === "scissors"))
-      ) {
+    ) {
         playerScore++;
         playerImg.src = `assets/images/${player}.png`;
         comImg.src = `assets/images/${computer}.png`;
+        if (playerScore === winningScore){
+            endGame("Player")
+        }
         return `You win! ${player} beats ${computer}.`;
-      }
-
-      computerScore++;
-      playerImg.src = `assets/images/${player}.png`;
-      comImg.src = `assets/images/${computer}.png`;
-      return `You lose! ${computer} beats ${player}.`;
-      
-    }
-    // plays the game, and runs through each function
-    
-    function handleClick(event) {
-      let playerSelection = event.target.id;
-      let computerSelection = computerPlay();
-      let result = playRound(playerSelection, computerSelection);
-      displayResult(playerSelection, computerSelection, result);
-      updateScore();
-    }
-//Code displays the moves for a screen reader to explain how and who won.
-    function displayResult(playerMove, computerMove, result) {
-      let playerMoveElement = document.getElementById("player-move");
-      let computerMoveElement = document.getElementById("computer-move");
-      let resultElement = document.getElementById("result");
-      
-
-      playerMoveElement.textContent = playerMove;
-      computerMoveElement.textContent = computerMove;
-      resultElement.textContent = result;
     }
 
-    // updates score board to keep track of win/loss
-    function updateScore() {
-      let playerScoreElement = document.getElementById("player-score");
-      let computerScoreElement = document.getElementById("computer-score");
-
-      playerScoreElement.textContent = playerScore;
-      computerScoreElement.textContent = computerScore;
+    computerScore++;
+    playerImg.src = `assets/images/${player}.png`;
+    comImg.src = `assets/images/${computer}.png`;
+    if (computerScore === winningScore) {
+        endGame("Computer")
     }
+    return `You lose! ${computer} beats ${player}.`;
 
-    // gives the buttons their functionality 
-    let buttons = document.querySelectorAll("button");
+}
+
+function endGame(winner) {
     buttons.forEach(button => {
-      button.addEventListener("click", handleClick);
-    });
+        button.disabled = true;
+    })
+    resetButton.disabled = false;
+    alert(`${winner} wins! Game Over.`)
+}
+// plays the game, and runs through each function
+
+function handleClick(event) {
+    let playerSelection = event.target.id;
+    let computerSelection = computerPlay();
+    let result = playRound(playerSelection, computerSelection);
+    displayResult(playerSelection, computerSelection, result);
+    updateScore();
+}
+//Code displays the moves for a screen reader to explain how and who won.
+function displayResult(playerMove, computerMove, result) {
+    let playerMoveElement = document.getElementById("player-move");
+    let computerMoveElement = document.getElementById("computer-move");
+    let resultElement = document.getElementById("result");
+
+
+    playerMoveElement.textContent = playerMove;
+    computerMoveElement.textContent = computerMove;
+    resultElement.textContent = result;
+}
+
+// updates score board to keep track of win/loss
+function updateScore() {
+    let playerScoreElement = document.getElementById("player-score");
+    let computerScoreElement = document.getElementById("computer-score");
+
+    playerScoreElement.textContent = playerScore;
+    computerScoreElement.textContent = computerScore;
+}
+
+// gives the buttons their functionality 
+
+buttons.forEach(button => {
+    button.addEventListener("click", handleClick);
+});
